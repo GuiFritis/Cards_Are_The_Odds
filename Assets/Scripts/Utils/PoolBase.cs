2fd6e28ce5cdfa4d92338e7d5a8cd67a9e0f3484
Gui Fritis <guilhermecfritis@gmail.com>
2025-02-16 10:20:50 -0300

    feat: :sparkles: import utils package

---
+++ Assets/Scripts/Utils/PoolBase.cs
@@ -0,0 +1,52 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Utils.Singleton;
+
+namespace Utils
+{
+    public abstract class PoolBase<T, S> : Singleton<S> where T : Component where S : MonoBehaviour
+    {
+        public int preWarmSize = 2;
+        public T PFB_item;
+
+        protected List<T> _pool = new();
+
+        protected override void Awake()
+        {
+            base.Awake();
+            InitPool();
+        }
+
+        private void InitPool()
+        {
+            _pool = new();
+
+            for (int i = 0; i < preWarmSize; i++)
+            {
+                CreatePoolItem();
+            }
+        }
+
+        private void CreatePoolItem()
+        {
+            T item = Instantiate(PFB_item, gameObject.transform);
+            _pool.Add(item);
+        }
+
+        public T GetPoolItem()
+        {
+            T item = null;
+            item = _pool.Find(CheckItem);
+            if(item == null)
+            {
+                CreatePoolItem();
+                item = _pool[^1];
+            }
+            return item;
+        }
+
+        protected abstract bool CheckItem(T item);
+    }
+}
